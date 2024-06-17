14618602940f1fcab6d6b18d64a8432121253f88
Jason Kwan <jasonkwan520@gmail.com>
2024-06-17 12:06:17 +0800

    Adding time count down for fixed period

--- App.tsx
+++ App.tsx
@@ -5,8 +5,12 @@
  * @format
  */
 
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import type {PropsWithChildren} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from "moment";
 import {
   SafeAreaView,
   ScrollView,
@@ -15,6 +19,9 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableOpacity,
+  Linking,
+  Button,
 } from 'react-native';
 
 import {
@@ -26,35 +33,118 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
-  title: string;
+  time: number;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+// Should be mutable by user
+let links = [
+  {
+    id: 1,
+    title: 'Wake up', // Link to wake up playlist
+    link: 'https://reactnative.dev/docs/tutorial',
+    time: 8,
+    timeLeft: "",
+    notified: false,
+  },
+  {
+    id: 2,
+    title: 'Start study',  // Link to React Native doc
+    link: 'https://reactnative.dev/docs/style',
+    time: 10,
+    timeLeft: "",
+    notified: false,
+  },
+  {
+    id: 3,
+    title: 'Lunch break',  // Link to open rice
+    link: 'https://reactnative.dev/docs/flexbox',
+    time: 12,
+    timeLeft: "",
+    notified: false,
+  },
+  {
+    id: 5,
+    title: 'Tea time',  // Link to open rice
+    link: 'https://reactnative.dev/docs/components-and-apis',
+    time: 15,
+    timeLeft: "",
+    notified: false,
+  },
+  {
+    id: 4,
+    title: 'Done for the day',    // Link to Youtube
+    link: 'https://reactnative.dev/docs/components-and-apis',
+    time: 18,
+    timeLeft: "",
+    notified: false,
+  },
+];
+
+function updateTime(){
+  state = {
+    dateString: moment(new Date()).format('YYYY-MM-DD'),
+    date: new Date(),
+    show: false
+  };
+
+onChange = (event: any, selectedDate: any) => {
+    console.log(selectedDate)
+    this.setState({dateString: moment(selectedDate).format('YYYY-MM-DD'), date: selectedDate})
+  }
+showOverlay = () => {
+    this.setState({ show: true})
+  }
+hideOverlay = () => {
+    this.setState({ show: false})
+  }
+
+    return (
+       <View style={{ flex: 1, borderRadius: 100}}>
+              <TouchableOpacity onPress={this.showOverlay} style={styles.inputContainerStyle}>
+                {this.state.dateString ? (
+                  <Text style={styles.textStyle}>{this.state.dateString}</Text>
+                ) : (
+                  <Text style={styles.placeholderStyle}>{this.props.placeholder}</Text>
+                )}
+              </TouchableOpacity>
+              {Platform.OS === 'ios' ? (
+                <Overlay isVisible={this.state.show} onBackdropPress={this.hideOverlay} overlayStyle={styles.overlayStyle}>
+                  <View style={styles.headerStyle}>
+                    <TouchableOpacity onPress={this.hideOverlay}>
+                      <Text style={{ paddingHorizontal: 15 }}>Cancel</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.hideOverlay}>
+                      <Text style={{ paddingHorizontal: 15, color: 'green' }}>Done</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <DateTimePicker
+                    value={this.state.date}
+                    mode={'date'}
+                    is24Hour={true}
+                    display="default"
+                    onChange={this.onChange}
+                    style={{ backgroundColor: 'white' }}
+                  />
+                </Overlay>
+              ) : (
+                <>
+                  {this.state.show &&
+                  <DateTimePicker
+                    value={this.state.date}
+                    mode={'date'}
+                    is24Hour={true}
+                    display="default"
+                    onChange={this.onChange}
+                    style={{ backgroundColor: 'white' }}
+                  />
+                  }
+                </>
+              )}
+            </View>
+          );
 }
 
+// main function
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -62,6 +152,34 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [displayText, setDisplayText] = useState(links);
+  setInterval(() => {
+    const currentTime = Date.now()
+    const currentDate = new Date(currentTime)
+    const currentHour = currentDate.getHours()
+    var updateString = ""
+    var hourLeft = 0
+    var presetDate = 0
+    var loop = 0;
+    // The preset date should be 18:00:00 everyday
+    // We may make it mutable in the future.
+
+     const updateText = displayText.map( blockInfo =>{
+        if(currentHour<links[loop].time) {
+           hourLeft = links[loop].time - (currentHour + 1)
+           presetDate  = new Date(currentDate.setHours(links[loop].time,0,0,0))
+           const recordedDate = new Date(presetDate - currentTime)
+           updateString =  "Time left"+"\n"+hourLeft+
+                           "hr "+recordedDate.getMinutes()+
+                           "mins "+recordedDate.getSeconds()+"sec"
+        } else {
+           updateString = "Done!!!"
+        }
+        links[loop].timeLeft = updateString
+        loop++
+     });
+     setDisplayText(updateText)
+    }, 1000);
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -71,30 +189,70 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
+        <Header/>
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <View style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+      }}>
+      {links.map(({id, title, link, timeLeft}) => (
+        <Fragment key={id}>
+          <TouchableOpacity
+            accessibilityRole="button"
+            onPress={() => Linking.openURL(link)}
+            style={linksStyles.linkContainer}>
+            <Text style={linksStyles.link}>{title}</Text>
+            <Text
+              style={[
+                linksStyles.description,
+                {
+                  color: isDarkMode ? Colors.white : Colors.black
+                  ,
+                },
+              ]}>
+              {timeLeft}
+            </Text>
+          </TouchableOpacity>
+        </Fragment>
+      ))}
+      </View>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+const linksStyles = StyleSheet.create({
+  container: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  linkContainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  link: {
+    flex: 2,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.primary,
+  },
+  description: {
+    flex: 3,
+    paddingVertical: 16,
+    textAlign: 'right',
+    fontWeight: '400',
+    fontSize: 18,
+  },
+  separator: {
+    height: StyleSheet.hairlineWidth,
+  },
+});
 
 const styles = StyleSheet.create({
   sectionContainer: {
